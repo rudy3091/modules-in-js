f735bc4be353155d9381720fcbdd616badc0e5be
rudy3091 <grayblack313@gmail.com>
2021-10-16 02:25:18 +0900

    Add commonjs

---
+++ commonjs/calculator.js
@@ -0,0 +1,11 @@
+exports.add = function (a, b) {
+  return a + b;
+}
+
+exports.sub = function (a, b) {
+  return a - b;
+}
+
+exports.mul = function (a, b) {
+  return a * b;
+}
---
+++ commonjs/index.js
@@ -0,0 +1,4 @@
+var calculator = require('./calculator');
+
+console.log('adding 3 and 4 =', calculator.add(3, 4));
+console.log('multiplying 8 and 3 =', calculator.mul(8, 3));
